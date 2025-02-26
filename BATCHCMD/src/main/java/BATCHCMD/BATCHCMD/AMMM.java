c6241b8f8ff5f765aac1565a3fa5093e8686721b
GurunathamCH <57170087+GurunathamCH@users.noreply.github.com>
2025-02-26 13:49:38 +0530

    Update AMMM.java
    new change in file

--- BATCHCMD/src/main/java/BATCHCMD/BATCHCMD/AMMM.java
+++ BATCHCMD/src/main/java/BATCHCMD/BATCHCMD/AMMM.java
@@ -4,8 +4,9 @@ public class AMMM {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("werew");
+System.out.println("sadsfhzxc");
 System.out.println("werew");		
+		
 	}
 
 }
